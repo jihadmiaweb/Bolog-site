917bb057bae396349560161c53a3c2f43951b46e
jihad mia <jihadmiaweb@gmail.com>
2025-10-06 13:04:20 +0600

    .

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+import router from '@/router/index.ts'
+import { ThemeProvider } from '@/provider/theme-provider'
+import { Toaster } from 'sonner'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
+  </StrictMode>,
+)
